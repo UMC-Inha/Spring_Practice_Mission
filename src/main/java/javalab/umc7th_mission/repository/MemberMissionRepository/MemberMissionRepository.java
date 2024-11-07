65a8f896a3c0170626c89eacfbea2a9f70d33be8
KYS <ehcl1027@gmail.com>
2024-11-07 11:46:46 +0900

    mission: submit week6 mission

---
+++ src/main/java/javalab/umc7th_mission/repository/MemberMissionRepository/MemberMissionRepository.java
@@ -0,0 +1,8 @@
+package javalab.umc7th_mission.repository.MemberMissionRepository;
+
+import javalab.umc7th_mission.domain.mapping.MemberMission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberMissionRepository extends JpaRepository<MemberMission, Long>, MemberMissionRepositoryCustom {
+
+}
