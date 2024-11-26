b9ec2000c1879d8a740ddbbc9c3af950787290b3
KYS <ehcl1027@gmail.com>
2024-11-26 14:25:31 +0900

    ✨ [feat] Store Review Inquiry API

--- src/main/java/javalab/umc7th_mission/web/dto/StoreResponseDTO.java
+++ src/main/java/javalab/umc7th_mission/web/dto/StoreResponseDTO.java
@@ -1,8 +1,17 @@
 package javalab.umc7th_mission.web.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class StoreResponseDTO {
+    //해당 클래스 -> 상점 추가시에 사용
     public static class AddResultDTO {
         private final Long storeId;
         private final LocalDateTime createdAt;
@@ -20,5 +29,29 @@ public class StoreResponseDTO {
         public LocalDateTime getCreatedAt() {
             return createdAt;
         }
+    }
+
+
+    //@Builder 대신 생성자 직접 주입
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreViewListDTO {
+        List<ReviewPreviewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirstPage;
+        Boolean isLastPage;
+    }
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewDTO {
+        String ownerNickname;
+        BigDecimal rating;
+        String body;
+        LocalDate createdAt;
     }
 }
