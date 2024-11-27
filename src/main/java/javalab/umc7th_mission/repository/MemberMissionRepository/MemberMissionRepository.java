611daaa1cf92d6407b1621b30d792b2729a26333
KYS <ehcl1027@gmail.com>
2024-11-27 22:39:33 +0900

    ✨ [feat] My Challenging Mission Inquiry API

--- src/main/java/javalab/umc7th_mission/repository/MemberMissionRepository/MemberMissionRepository.java
+++ src/main/java/javalab/umc7th_mission/repository/MemberMissionRepository/MemberMissionRepository.java
@@ -2,11 +2,20 @@ package javalab.umc7th_mission.repository.MemberMissionRepository;
 
 import javalab.umc7th_mission.domain.enums.MissionStatus;
 import javalab.umc7th_mission.domain.mapping.MemberMission;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MemberMissionRepository extends JpaRepository<MemberMission, Long>, MemberMissionRepositoryCustom {
 
     boolean existsByMemberIdAndMissionIdAndStatus(Long memberId, Long missionId, MissionStatus status);
+
+    @Query("SELECT mm FROM MemberMission mm " +
+            "WHERE mm.member.id = :memberId " +
+            "AND mm.status = javalab.umc7th_mission.domain.enums.MissionStatus.CHALLENGING")
+    Page<MemberMission> findChallengingMissionByMemberId(@Param("memberId") Long memberId, PageRequest request);
 }
